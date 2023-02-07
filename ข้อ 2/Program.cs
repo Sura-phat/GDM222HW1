4c5c4b2f6b2fcd2382af89d58bc9389071799417
Sura-phat <suraphat.phu@mail.kmutt.ac.th>
2023-02-07 22:55:46 +0700

    1

--- ข้อ 2/Program.cs
+++ ข้อ 2/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+    static void Main()
+    {        
+        double inputXC = double.Parse(Console.ReadLine());
+        double inputYC = double.Parse(Console.ReadLine());
+        double inputXP = double.Parse(Console.ReadLine());
+        double inputYP = double.Parse(Console.ReadLine());
+        Circle(inputXC,inputYC,inputXP,inputYP);
+    }
+    static void Circle
+                (double XC, 
+                 double YC, 
+                 double XP, 
+                 double YP)
+    {
+        double NewXC = 0;
+        double NewYC = 0;
+        double radian = Math.Atan((YP-YC)/(XP-XC));
+        double distance = Math.Sqrt(Math.Pow(YP-YC,2)+Math.Pow(XP-XC,2));
+    }
+}
