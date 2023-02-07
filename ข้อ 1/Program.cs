613e572b751b642b74e81626aa513ed50da2c278
Sura-phat <suraphat.phu@mail.kmutt.ac.th>
2023-02-07 22:17:38 +0700

    first commit

---
+++ ข้อ 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    { 
+        double inputnumber = double.Parse(Console.ReadLine());
+        Console.WriteLine(calculator(inputnumber));  
+    }
+    static double calculator(double inputnumber)
+    {
+        double Answer = 0;
+      
+        if(Math.Pow(inputnumber, 2) > 4)
+        {
+            Answer = 1 + calculator(inputnumber-(4 * Math.Abs(inputnumber))/inputnumber);
+            return Answer;
+        }
+      
+        else 
+        {
+            return Answer = 2;
+        }
+    }
+}
---
+++ ข้อ 3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        double inputnumber = double.Parse(Console.ReadLine());
+        Console.WriteLine(calculator(inputnumber));
+    }
+    static double calculator(double inputnumber)
+    {
+        double solution = 0;
+        double Answer = 0;
+        
+        if (inputnumber >-1 && inputnumber<=0)
+        {
+            solution = 1 - Math.Pow(inputnumber,2);
+            Answer = Math.Sqrt(solution);
+            return Answer;
+        }
+        
+        if (inputnumber >0 && inputnumber<=1)
+        {
+            solution = 1 - Math.Pow(inputnumber,2);
+            Answer = -1 * Math.Sqrt(solution);
+            return Answer; 
+        }
+        
+        if (inputnumber <= -1)
+        {
+            Answer = calculator(inputnumber + 2);
+            return Answer;
+        }
+       
+        else
+        {
+            Answer = calculator(inputnumber - 2);
+            return Answer;
+        }
+    }
+}
